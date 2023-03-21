623335846a9aa9aec2ad24b0ff86d4c3b9f4eacc
Mickael972 <madallstar@gmail.com>
2023-03-21 11:23:04 +0100

    ajout du formulaire de connexion

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Connection Form</title>
+    <link rel="icon" href="/assets/img/logo.php.png">
+  </head>
+  <body>
+    <h1>Connection Form</h1>
+    <form method="post" action="connection.php">
+      <label for="email">Email :</label>
+      <input type="email" name="email" id="email" required><br><br>
+      <label for="password">Password :</label>
+      <input type="password" name="password" id="password" required><br><br>
+      <input type="submit" value="Get Connect">
+    </form>
+  </body>
+</html>

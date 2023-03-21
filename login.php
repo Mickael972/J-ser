ea215c5f9d695d3abe2dd340430d3a28d2043fdd
Mickael972 <madallstar@gmail.com>
2023-03-21 15:21:06 +0100

    login finished

--- login.php
+++ login.php
@@ -1,14 +1,19 @@
 <?php require_once __DIR__. '/layout/header.php';?>
-<div class="container-fluid text-light text-center mt-5 d-flex align-items-center justify-content-center" id="custom-background">
-    <div>   
-            <h1 class="my-5">Connection Form</h1>
-            <form method="post" action="connection.php">
-                <label for="email">Email :</label>
-                <input type="email" name="email" id="email" required><br><br>
-                <label for="password">Password :</label>
-                <input type="password" name="password" id="password" required><br><br>
-                <input type="submit" value="Get Connect">
-            </form>
-        </div>
+
+<div class="container-fluid text-light d-flex align-items-center flex-column justify-content-center" id="custom-background">
+    <h1 class="text-center mt-5">Login</h1>
+    <form class="d-flex flex-column justify-content-center align-items-center" method="post" action="connection.php">
+    <div class="mb-3">
+        <label for="email" class="form-label">Email :</label>
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password :</label>
+        <input type="password" name="password" class="form-control" id="password" required>
+    </div>
+        <button type="submit" class="btn btn-primary">Get Connect</button>
+    </form>
 </div>
+    
+    
 <?php require_once __DIR__.'/layout/footer.php';?>

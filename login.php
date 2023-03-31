cd1d740e23dd64fe6ca745f202a07a9ad0889cd9
Mickael972 <madallstar@gmail.com>
2023-03-31 12:01:30 +0200

    finition de register.php

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?php require_once __DIR__ . '/layout/header.php'; ?>
 
-<div class="container-fluid text-light d-flex align-items-center flex-column justify-content-center" id="custom-background">
+<div class="container-fluid text-light d-flex align-items-center flex-column justify-content-center custom-background">
     <h1 class="text-center mt-5">Login</h1>
-    <form class="d-flex flex-column justify-content-center align-items-center" method="post" action="connection_login.php">
+
+    <form class="d-flex flex-column justify-content-center align-items-center" action="login_process.php" method="post">
         <div class="mb-3">
-            <label for="email" class="form-label">Name :</label>
-            <input type="text" name="name" class="form-control" id="email" aria-describedby="emailHelp" required>
+            <label for="email" class="form-label">Email address :</label>
+            <input type="text" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="name@example.com" required>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Password :</label>
-            <input type="password" name="password" class="form-control" id="password" required>
+            <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
         </div>
-        <button type="submit" class="btn btn-primary">Get Connect</button>
+        <button type="submit" class="btn btn-primary">Log In</button>
     </form>
 </div>
 
